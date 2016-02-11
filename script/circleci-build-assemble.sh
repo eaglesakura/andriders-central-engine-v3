ccda33d847d280fb59d04be89a4081142f31cc20
eaglesakura <eagle.sakura@gmail.com>
2016-02-11 22:59:29 +0900

    ビルドスクリプトの処理を分割

--- script/circleci-build-assemble.sh
+++ script/circleci-build-assemble.sh
@@ -1,7 +1,21 @@
 #! /bin/sh
 
 # ビルド
-./gradlew ciClean clean assembleDebug assembleRelease
+./gradlew ciClean clean
+
+if [ $? -ne 0 ]; then
+    echo "build failed..."
+    exit 1
+fi
+
+./gradlew assembleRelease
+
+if [ $? -ne 0 ]; then
+    echo "build failed..."
+    exit 1
+fi
+
+./gradlew assembleDebug
 
 if [ $? -ne 0 ]; then
     echo "build failed..."
