f993722e8a967ccb8cf979ad3b73214e3d6c00f3
eaglesakura <eagle.sakura@gmail.com>
2016-07-10 00:39:25 +0900

    HeaderViewのプレビューを行うためのワークアラウンドを追加 refs #82

--- app/src/main/java/com/eaglesakura/andriders/ui/widget/AppHeaderView.java
+++ app/src/main/java/com/eaglesakura/andriders/ui/widget/AppHeaderView.java
@@ -7,13 +7,20 @@ import com.eaglesakura.android.util.ContextUtil;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AppHeaderView extends FrameLayout {
+    ImageView mIcon;
+
+    TextView mTitle;
+
     public AppHeaderView(Context context) {
         super(context);
         init(context, null, 0);
@@ -32,19 +39,23 @@ public class AppHeaderView extends FrameLayout {
 
     void init(Context context, AttributeSet attrs, int defStyleAttr) {
         LayoutInflater inflater = ContextUtil.getInflater(context);
-
-        View view = inflater.inflate(R.layout.view_item_header, null);
+        View view = inflater.inflate(R.layout.view_item_header, this);
 
         TypedArray typedArray = context.obtainStyledAttributes(attrs, new int[]{
                 R.attr.headerIcon,
                 R.attr.headerText
         });
 
-        new AQuery(view)
-                .id(R.id.App_HeaderView_Icon).image(typedArray.getDrawable(0))
-                .id(R.id.App_HeaderView_Title).text(typedArray.getString(1));
-        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        addView(view, params);
-        invalidate();
+        mIcon = (ImageView) findViewById(R.id.App_HeaderView_Icon);
+        mTitle = (TextView) findViewById(R.id.App_HeaderView_Title);
+
+        if (isInEditMode()) {
+            // ワークアラウンド
+            mIcon = (ImageView) ((ViewGroup) ((ViewGroup) view).getChildAt(0)).getChildAt(0);
+            mTitle = (TextView) ((ViewGroup) ((ViewGroup) view).getChildAt(0)).getChildAt(1);
+        }
+
+        mIcon.setImageDrawable(typedArray.getDrawable(0));
+        mTitle.setText(typedArray.getString(1));
     }
 }
