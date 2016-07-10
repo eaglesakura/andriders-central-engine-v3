97c1918e330b3723ba00dc60f334958afbe7f3dc
eaglesakura <eagle.sakura@gmail.com>
2016-07-11 00:22:10 +0900

    UnitTestコマンドを変更 refs #10

--- script/circleci-build-testing.sh
+++ script/circleci-build-testing.sh
@@ -4,19 +4,20 @@
 report_cp() {
 mkdir "$CIRCLE_TEST_REPORTS/junit/"
 mkdir "$CIRCLE_TEST_REPORTS/junit/googleplayDebug"
-mkdir "$CIRCLE_TEST_REPORTS/junit/googleplayRelease"
 
 find . -type f -regex ".*/build/test-results/googleplayDebug/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/junit/googleplayDebug/ \;
-find . -type f -regex ".*/build/test-results/googleplayRelease/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/junit/googleplayRelease/ \;
 
 mkdir "$CIRCLE_ARTIFACTS/app"
-mkdir "$CIRCLE_ARTIFACTS/sdk"
 cp -r ./app/build/reports "$CIRCLE_ARTIFACTS/app"
-cp -r ./sdk/build/reports "$CIRCLE_ARTIFACTS/sdk"
+
+#mkdir "$CIRCLE_TEST_REPORTS/junit/googleplayRelease"
+#find . -type f -regex ".*/build/test-results/googleplayRelease/.*xml" -exec cp {} $CIRCLE_TEST_REPORTS/junit/googleplayRelease/ \;
+#mkdir "$CIRCLE_ARTIFACTS/sdk"
+#cp -r ./sdk/build/reports "$CIRCLE_ARTIFACTS/sdk"
 }
 
 # テスト実行
-./gradlew test
+./gradlew  -Dorg.gradle.daemon=false -Dorg.gradle.jvmargs="-Xmx2048m -XX:MaxPermSize=1024m -XX:+HeapDumpOnOutOfMemoryError" testGoogleplayDebugUnitTest
 
 
 if [ $? -ne 0 ]; then
