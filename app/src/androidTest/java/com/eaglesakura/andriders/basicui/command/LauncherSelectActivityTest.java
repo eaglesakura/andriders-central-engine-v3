cda7b09a88135d1612d6ed28d5fdb3c66c76f195
eaglesakura <eagle.sakura@gmail.com>
2016-07-16 23:57:38 +0900

    アプリ選択Activityの戻り値チェックTestを追加 refs #4

--- app/src/androidTest/java/com/eaglesakura/andriders/basicui/command/LauncherSelectActivityTest.java
+++ app/src/androidTest/java/com/eaglesakura/andriders/basicui/command/LauncherSelectActivityTest.java
@@ -2,13 +2,28 @@ package com.eaglesakura.andriders.basicui.command;
 
 import com.eaglesakura.andriders.AceApplication;
 import com.eaglesakura.andriders.AppDeviceTestUtil;
+import com.eaglesakura.andriders.R;
+import com.eaglesakura.andriders.command.CommandKey;
+import com.eaglesakura.andriders.command.CommandSetting;
+import com.eaglesakura.andriders.db.command.CommandSetupData;
 import com.eaglesakura.andriders.util.AppLog;
 import com.eaglesakura.android.devicetest.DeviceActivityTestCase;
+import com.eaglesakura.android.thread.ui.UIHandler;
+import com.eaglesakura.material.widget.adapter.AdapterCollection;
 import com.eaglesakura.util.Util;
 
 import org.junit.Test;
 
+import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerViewAccessibilityDelegate;
 
 import java.util.List;
 
@@ -43,6 +58,36 @@ public class LauncherSelectActivityTest extends DeviceActivityTestCase<LauncherS
         LauncherSelectActivity activity = getActivity();
         Util.sleep(1000);
         assertNotEquals(activity.mAdapter.getCollection().size(), 0);
+    }
+
+    @Test
+    public void 生成されたIntentが正常な戻り値として構築されている() throws Throwable {
+        Intent bootIntent = new Intent().putExtra(CommandSetting.EXTRA_COMMAND_KEY, CommandKey.fromProximity(1));
+        LauncherSelectActivity activity = getActivity(bootIntent);
+        Util.sleep(1000);
+
+        UIHandler.await(() -> {
+            AdapterCollection<ResolveInfo> collection = activity.mAdapter.getCollection();
+            Intent intent = activity.onItemSelected(collection.get(0));
+            assertTrue(activity.isFinishing());
+            assertNotNull(intent);
+
+            CommandSetupData result = CommandSetupData.getFromResult(intent);
+            assertNotNull(result);
+
+            return 0;
+        });
+    }
+
+    @Test
+    public void アイテムがタップされたらfinishされる() throws Throwable {
+        LauncherSelectActivity activity = getActivity();
+        Util.sleep(1000);
+        Espresso.onView(ViewMatchers.isAssignableFrom(RecyclerView.class))
+                .perform(RecyclerViewActions.scrollToPosition(20))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(20, ViewActions.click()));
+        Util.sleep(500);
+        assertTrue(activity.isFinishing());
     }
 
     @Test
