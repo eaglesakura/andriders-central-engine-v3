4717b357d82fc26a729cf5a1489a16594d5f32c0
eaglesakura <eagle.sakura@gmail.com>
2016-09-07 23:11:14 +0900

    タイヤ周長データを追加、ライブラリ名がリネームされてしまっていたので修正 refs #112

---
+++ app/src/main/java/com/eaglesakura/andriders/db/RoadbikeWheelLengthCollection.java
@@ -0,0 +1,16 @@
+package com.eaglesakura.andriders.db;
+
+public class RoadbikeWheelLengthCollection {
+
+    public static class Model {
+        /**
+         * 表示名
+         */
+        public String name;
+
+        /**
+         * タイヤ周長 mm
+         */
+        public int length;
+    }
+}
