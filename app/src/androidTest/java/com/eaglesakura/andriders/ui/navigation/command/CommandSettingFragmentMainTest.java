88bde8a772c195047b9b88848d28b66c04209450
eaglesakura <eagle.sakura@gmail.com>
2016-09-17 00:49:15 +0900

    DeviceTestのビルドエラーを修正

--- app/src/androidTest/java/com/eaglesakura/andriders/ui/navigation/command/CommandSettingFragmentMainTest.java
+++ app/src/androidTest/java/com/eaglesakura/andriders/ui/navigation/command/CommandSettingFragmentMainTest.java
@@ -1,16 +1,17 @@
 package com.eaglesakura.andriders.ui.navigation.command;
 
 import com.eaglesakura.andriders.ui.navigation.NavigationActivityTest;
-import com.eaglesakura.andriders.ui.navigation.NavigationBaseFragment;
 
 import org.junit.Test;
+
+import static com.eaglesakura.android.devicetest.scenario.ScenarioContext.findFragment;
 
 public class CommandSettingFragmentMainTest extends NavigationActivityTest {
 
     @Test
     public void 正常にFragmentが起動できる() throws Exception {
-        NavigationBaseFragment fragment = getNavigationFragment(TargetStub.class);
-        assertNotNull(fragment);
+        validate(findFragment(CommandSettingFragmentMain.class))
+                .notNull();
     }
 
     public static class TargetStub extends CommandSettingFragmentMain {
