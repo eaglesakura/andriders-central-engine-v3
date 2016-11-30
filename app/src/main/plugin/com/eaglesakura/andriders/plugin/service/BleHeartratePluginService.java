86a76abe2af351f04ab97a7e17cdf63dfcc2b006
eaglesakura <eagle.sakura@gmail.com>
2016-11-30 23:51:51 +0900

    BLE心拍センサーを復旧 refs #140

--- app/src/main/plugin/com/eaglesakura/andriders/plugin/service/BleHeartratePluginService.java
+++ app/src/main/plugin/com/eaglesakura/andriders/plugin/service/BleHeartratePluginService.java
@@ -1,20 +1,21 @@
-package com.eaglesakura.andriders.ble.service;
+package com.eaglesakura.andriders.plugin.service;
 
 import com.eaglesakura.andriders.ble.heartrate.BleHeartrateMonitor;
 import com.eaglesakura.andriders.ble.heartrate.HeartrateGattReceiver;
 import com.eaglesakura.andriders.ble.heartrate.HeartrateSensorData;
 import com.eaglesakura.andriders.plugin.AcePluginService;
 import com.eaglesakura.andriders.plugin.Category;
-import com.eaglesakura.andriders.plugin.CentralEngineConnection;
 import com.eaglesakura.andriders.plugin.DisplayKey;
 import com.eaglesakura.andriders.plugin.PluginInformation;
-import com.eaglesakura.andriders.plugin.data.CentralEngineData;
+import com.eaglesakura.andriders.plugin.connection.PluginConnection;
+import com.eaglesakura.andriders.plugin.data.CentralEngineSessionData;
 import com.eaglesakura.andriders.sensor.SensorType;
-import com.eaglesakura.andriders.service.base.AppBaseService;
 import com.eaglesakura.andriders.util.AppLog;
 import com.eaglesakura.andriders.util.Clock;
+import com.eaglesakura.android.framework.delegate.lifecycle.ServiceLifecycleDelegate;
 import com.eaglesakura.util.StringUtil;
 
+import android.app.Service;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.IBinder;
@@ -22,7 +23,7 @@ import android.support.annotation.Nullable;
 
 import java.util.List;
 
-public class BleHeartrateExtensionService extends AppBaseService implements AcePluginService {
+public class BleHeartratePluginService extends Service implements AcePluginService {
     HeartrateGattReceiver receiver;
 
     /**
@@ -30,27 +31,29 @@ public class BleHeartrateExtensionService extends AppBaseService implements AceP
      */
     private final Clock mClock = Clock.getRealtimeClock();
 
+    ServiceLifecycleDelegate mLifecycleDelegate = new ServiceLifecycleDelegate();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        AppLog.system("onBind(%s) : %s", intent.getAction(), toString());
-        CentralEngineConnection session = CentralEngineConnection.onBind(this, intent);
+        AppLog.system("onBind(%s)", toString());
+        PluginConnection session = PluginConnection.onBind(this, intent);
         if (session == null) {
             return null;
         }
+
         return session.getBinder();
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        AppLog.system("onUnbind(%s) : %s", intent.getAction(), toString());
-        CentralEngineConnection.onUnbind(this, intent);
+        AppLog.system("onUnbind(%s)", toString());
+        PluginConnection.onUnbind(this, intent);
         return super.onUnbind(intent);
     }
 
-
     @Override
-    public PluginInformation getExtensionInformation(CentralEngineConnection connection) {
+    public PluginInformation getExtensionInformation(PluginConnection connection) {
         PluginInformation info = new PluginInformation(this, "ble_hr");
         info.setSummary("Bluetooth LE対応センサーから心拍を取得します");
         info.setCategory(Category.CATEGORY_HEARTRATEMONITOR);
@@ -58,19 +61,21 @@ public class BleHeartrateExtensionService extends AppBaseService implements AceP
     }
 
     @Override
-    public List<DisplayKey> getDisplayInformation(CentralEngineConnection connection) {
+    public List<DisplayKey> getDisplayInformation(PluginConnection connection) {
         return null;
     }
 
     @Override
-    public void onAceServiceConnected(CentralEngineConnection connection) {
-        final CentralEngineData centralDataExtension = connection.getCentralDataExtension();
-        String address = centralDataExtension.getGadgetAddress(SensorType.HeartrateMonitor);
+    public void onAceServiceConnected(PluginConnection connection) {
+        final CentralEngineSessionData centralData = connection.getCentralData();
+        String address = centralData.getGadgetAddress(SensorType.HeartrateMonitor);
+        AppLog.ble("BLE HeartrateSensor[%s]", address);
         if (StringUtil.isEmpty(address)) {
             return;
         }
 
-        receiver = new HeartrateGattReceiver(this, getCallbackQueue(), mClock);
+        mLifecycleDelegate.onCreate();
+        receiver = new HeartrateGattReceiver(this, mLifecycleDelegate.getCallbackQueue(), mClock);
         receiver.setTargetFitnessDeviceAddress(address);
         receiver.setHeartrateListener(new BleHeartrateMonitor.BleHeartrateListener() {
             @Override
@@ -83,7 +88,7 @@ public class BleHeartrateExtensionService extends AppBaseService implements AceP
 
             @Override
             public void onHeartrateUpdated(BleHeartrateMonitor sensor, HeartrateSensorData heartrate) {
-                centralDataExtension.setHeartrate(heartrate.getBpm());
+                centralData.setHeartrate(heartrate.getBpm());
             }
         });
 
@@ -91,25 +96,28 @@ public class BleHeartrateExtensionService extends AppBaseService implements AceP
     }
 
     @Override
-    public void onAceServiceDisconnected(CentralEngineConnection connection) {
+    public void onAceServiceDisconnected(PluginConnection connection) {
         if (receiver != null) {
             receiver.disconnect();
             receiver = null;
         }
+
+
+        mLifecycleDelegate.onDestroy();
     }
 
     @Override
-    public void onEnable(CentralEngineConnection connection) {
+    public void onEnable(PluginConnection connection) {
 
     }
 
     @Override
-    public void onDisable(CentralEngineConnection connection) {
+    public void onDisable(PluginConnection connection) {
 
     }
 
     @Override
-    public void startSetting(CentralEngineConnection connection) {
+    public void startSetting(PluginConnection connection) {
 
     }
 
