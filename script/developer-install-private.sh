e4e40a2bde646b421b031a486da552fd83ba6b4e
eaglesakura <eaglesakura@SQUALL.local>
2017-08-05 20:55:09 +0900

    privateファイルの配置を既存プロジェクトに合わせて変更 refs #200

--- script/developer-install-private.sh
+++ script/developer-install-private.sh
@@ -1,27 +1,18 @@
 #! /bin/sh
 
+mkdir private
+rm -rf private/ace-private/
+
 #################################################################################
 # ACE開発者がprivateファイルを同期するためのスクリプト
 # OSS化に伴い、プライベートリポジトリで署名鍵等の重要ファイルを管理する
 # ace-privateを同期後、必要なファイルをローカルにコピーする
 #################################################################################
-if [ -e ./ace-private/v3.0.x ]; then
-    echo "installed ace-private/"
-    cd ace-private/
-    git fetch
-    git pull origin master
-    cd ..
-else
-    git clone git@bitbucket.org:eaglesakura/ace-private.git
-fi
+git clone git@bitbucket.org:eaglesakura/ace-private.git private/ace-private
 
-if [ -e "./ace-private/v3.0.x/" ]; then
-    cp -rf ./ace-private/v3.0.x/main           ./app/private/
-    cp -rf ./ace-private/v3.0.x/sign           ./app/private/
-    cp -f  ./ace-private/v3.0.x/private.gradle ./app/private/
-    cp -rf ./ace-private/v3.0.x/debug          ./app/src/
-    cp -rf ./ace-private/v3.0.x/release        ./app/src/
+if [ -e "./private/ace-private/v3.0.x/" ]; then
+    cp -rf ./private/ace-private/v3.0.x/** .
 else
     echo "ace-private not sync"
-    cp -rf ./app/private.tmp/** ./app/private/
+    cp -rf private.template/** .
 fi
